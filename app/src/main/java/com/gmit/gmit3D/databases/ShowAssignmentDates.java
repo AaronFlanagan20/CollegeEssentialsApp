86d0e2cca07875ae401b819f82ffddf4e67ee58b
Aaron Flanagan <aaronflanagan044@hotmail.com>
2016-01-14 19:05:23 +0000

    Countdowntimer for assignments working

--- app/src/main/java/com/gmit/gmit3D/databases/ShowAssignmentDates.java
+++ app/src/main/java/com/gmit/gmit3D/databases/ShowAssignmentDates.java
@@ -2,6 +2,7 @@ package com.gmit.gmit3D.databases;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -24,16 +25,61 @@ public class ShowAssignmentDates extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_assignment_dates);
 
-        ad = new ApplicationDatabase(getBaseContext());
+        ad = new ApplicationDatabase(getBaseContext());//get database object
         ad.createDatabase();
 
         view1 = (TextView) findViewById(R.id.moduleText);
-
+        view1.setText("Due");
         try {
             Cursor cursor = ad.returnAssignmentData();//cursor returns everythig from result set
             if(cursor.moveToFirst()){
-                view1.setText("ASDSSD" + " " + cursor.getString(0));
+                String name = cursor.getString(0);
+                String date = cursor.getString(1);
 
+                //begin split
+                String[] seperate = date.split("/");
+                String day = seperate[0];
+                String month = seperate[1];
+                date = seperate[2];//date will equal year SPACE time
+
+                //must split again because 2016 13:00
+                seperate = date.split(" ");//split space between year and time of day
+                String year = seperate[0];
+                String time = seperate[1];
+
+                //begin spliting time
+                seperate = time.split(":");
+                String hour = seperate[0];//eg 12, 1, 16
+                String minute = seperate[1];
+
+                //get integer values of strings
+                int minuteInt = Integer.parseInt(minute);
+                int hourInt = Integer.parseInt(hour);
+                int yearInt = Integer.parseInt(year);
+                int monthInt = Integer.parseInt(month);
+                int dayInt = Integer.parseInt(day);
+
+                ct = new AssignmentCountdownTimer(0, minuteInt, hourInt, dayInt, monthInt, yearInt);
+                CountDownTimer countDownTimer = new CountDownTimer(ct.getIntervalMillis(), 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        int days = (int) ((millisUntilFinished / 1000) / 86400);
+                        int hours = (int) (((millisUntilFinished / 1000)
+                                - (days * 86400)) / 3600);
+                        int minutes = (int) (((millisUntilFinished / 1000)
+                                - (days * 86400) - (hours * 3600)) / 60);
+                        int seconds = (int) ((millisUntilFinished / 1000) % 60);
+
+                        String countdown = String.format("%02dd %02dh %02dm %02ds", days,
+                                hours, minutes, seconds);
+                        view1.setText(countdown);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        view1.setText("Time is up, Assignment due date is upon us");
+                    }
+                }.start();
             }
         }catch (NullPointerException e){
             e.printStackTrace();
@@ -41,3 +87,8 @@ public class ShowAssignmentDates extends AppCompatActivity {
     }
 
 }
+
+
+//seperate[0] is day
+//seperate[1] is month
+//seperate[2] is year + time
