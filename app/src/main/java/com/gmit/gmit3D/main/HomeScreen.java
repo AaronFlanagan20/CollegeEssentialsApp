858fd693e6c6e66b642554a8282b8a509620e799
= <ciaranbrennan1992@gmail.com>
2016-01-25 14:24:08 +0000

    Google maps icon added

--- app/src/main/java/com/gmit/gmit3D/main/HomeScreen.java
+++ app/src/main/java/com/gmit/gmit3D/main/HomeScreen.java
@@ -1,6 +1,8 @@
 package com.gmit.gmit3D.main;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,7 +12,7 @@ import com.gmit.gmit3D.database.ApplicationDatabase;
 
 public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton cameraButton, timetableButton, assignmentButton, aboutButton;
+    private ImageButton cameraButton, timetableButton, assignmentButton, aboutButton, mapButton;
     ApplicationDatabase ad;
 
     /* called when activity is first created*/
@@ -30,6 +32,9 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
 
         aboutButton = (ImageButton)findViewById(R.id.about);//represents locate about on home screen
         aboutButton.setOnClickListener(this);
+
+        mapButton = (ImageButton)findViewById(R.id.mapButton);//link to gmaps
+        mapButton.setOnClickListener(this);
 
         ad = new ApplicationDatabase(getBaseContext());
         ad.createDatabase();
@@ -51,6 +56,20 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
         startActivity(new Intent("com.gmit.gmit3D.main.About"));//open about activity
     }
 
+    private Intent openMap(Context context){
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.google.android.maps.MapView", 0);
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://facewebmodal/f?href=https://www.facebook.com/GMITOfficial"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.google.ie/maps/place/GMIT/@53.2786241,-9.0127533,17z/data=!3m1!4b1!4m2!3m1!1s0x485b9132cb2e133f:0x5a81260c20e02143"));
+        }
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){//used to check for all buttons
@@ -65,6 +84,9 @@ public class HomeScreen extends AppCompatActivity implements View.OnClickListene
                 break;
             case R.id.about://open assignment activity
                 openAbout();
+                break;
+            case R.id.mapButton:
+                openMap(this);
                 break;
         }
     }
